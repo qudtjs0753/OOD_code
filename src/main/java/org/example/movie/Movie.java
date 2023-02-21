ed7212c6bf92235751a4c181b6811466f6923823
qudtjs0753 <qudtjs0753@naver.com>
2023-02-21 20:08:42 +0900

    refactor: Movie class 수정

--- src/main/java/org/example/movie/Movie.java
+++ src/main/java/org/example/movie/Movie.java
@@ -20,9 +20,6 @@ public class Movie {
     }
 
     public Money calculateMovieFee(Screening screening) {
-        if (discountPolicy == null) {
-            return fee;
-        }
         return fee.minus(discountPolicy.calculateDiscountAmount(screening));
     }
 
